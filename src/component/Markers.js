ddf70fe1b9429d7ea6154b64a779385bc09ab6c4
MateWagner <wwagnermate@gmail.com>
2023-10-24 16:07:36 +0200

    feat: line color moved to a variable

--- src/component/Markers.js
+++ src/component/Markers.js
@@ -1,9 +1,10 @@
 import { Marker } from "react-map-gl";
 const Markers = ({ coordinatesList }) => {
+  const LINE_COLOR = "red";
   return (
     <>
       {coordinatesList.map((coordinates, index) => (
-        <Marker key={index} {...coordinates} color="red" />
+        <Marker key={index} {...coordinates} color={LINE_COLOR} />
       ))}
     </>
   );
